8a9eb3c516bdb1479aa3db3b48f1ea1e4c38ae19
ixotum <ixotum@gmail.com>
2012-08-18 14:29:04 +0300

    MINOR CHANGE: File description added

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,10 @@
+/*******************************************************************************
+  FILE: main.cpp
+  AUTOR: ixotum
+  DATE: 2012-08-18 14:25:59
+  DESCRIPTION: This file contains main function
+*******************************************************************************/
+
 #include <QtGui/QApplication>
 
 #include "mainwindow.h"
--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,3 +1,10 @@
+/*******************************************************************************
+  FILE: mainwindow.cpp
+  AUTOR: ixotum
+  DATE: 2012-08-18 14:27:37
+  DESCRIPTION: This file contains implementation of MainWindow class
+*******************************************************************************/
+
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
