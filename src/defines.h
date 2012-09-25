b06b39d93707bc1ff1e1780963f45ab24ea6d346
ixotum <ixotum@gmail.com>
2012-09-25 20:38:44 +0300

    MAJOR CHANGE: updateDOM() implemented
    Implementation in first redaction added.

--- src/defines.h
+++ src/defines.h
@@ -10,7 +10,9 @@
 #define KSILIT_JOTTER_ACTION_ADD_SUB_NOTE_TEXT "Add Sub Note"
 #define KSILIT_JOTTER_ACTION_ADD_NOTE_TEXT     "Add Note"
 #define KSILIT_JOTTER_SUB_NOTE_TEXT            "New Note"
+#define KSILIT_JOTTER_DOM_ELEMENT_TAG          "Note"
 
-#define ERROR_OPENING_FILE 1
+#define ERROR_UNKNOWN_ERROR 1
+#define ERROR_OPENING_FILE  2
 
 #endif // DEFINES_H
